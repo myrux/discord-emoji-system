44b78bc9ccff19782bccb2698950fd2722149c23
Muhammet Emin <66025604+MuhammetDev@users.noreply.github.com>
2024-06-22 18:26:51 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+{
+  "token": "MTI0NjQzNjU4OTU3MjE5NDM4NA.Go5oRv.bPZ2uj29YWrJaLzDDaivEgb-apCvzrLeHzh_6Y ",
+  "clientId": "1239638957642481714", 
+  "prefix": "!", 
+  "sahip": "1239245942452912281"
+}
