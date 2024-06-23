9661759dfbdd029df1c46ca651675cc68587b363
Muhammet Emin <66025604+MuhammetDev@users.noreply.github.com>
2024-06-23 09:37:42 +0300

    Update davet.js

--- commands/davet.js
+++ commands/davet.js
@@ -1,1 +1,31 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const config = require('../config.json');
+
+module.exports = {
+    name: 'invite',
+    description: 'Provides the bot invite link and support server link.',
+    execute(message, args, locale) {
+        const inviteLink = `https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=8&scope=bot%20applications.commands`;
+        const supportServerLink = config.supportServerLink;
+
+        const embed = new EmbedBuilder()
+            .setTitle(locale.invite.title)
+            .setDescription(locale.invite.description)
+            .setColor('#0099ff');
+
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setLabel(locale.invite.inviteButton)
+                    .setURL(inviteLink)
+                    .setStyle(ButtonStyle.Link),
+                new ButtonBuilder()
+                    .setLabel(locale.invite.supportButton)
+                    .setURL(supportServerLink)
+                    .setStyle(ButtonStyle.Link),
+            );
+
+        message.reply({ embeds: [embed], components: [row] });
+    },
+};
 
