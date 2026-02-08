d754cd64972e450dcb36da46722295c17f332639
myrux <66025604+myrux@users.noreply.github.com>
2026-02-08 21:26:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 {
-  "token": "MTI0NjQzNjU4OTU3MjE5NDM4NA.Go5oRv.bPZ2uj29YWrJaLzDDaivEgb-apCvzrLeHzh_6Y ",
+  "token": "",
   "clientId": "1239638957642481714", 
   "prefix": "!", 
   "sahip": "1239245942452912281"
